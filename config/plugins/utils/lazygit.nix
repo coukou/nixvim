6c517955ea58648f11296a6ebdc0f8b32e3c6769
coukou <orenkay57@gmail.com>
2025-08-15 21:59:10 +0200

    feat(utils/lazygit): add <leader>lg keymap to open LazyGit in normal mode

--- config/plugins/utils/lazygit.nix
+++ config/plugins/utils/lazygit.nix
@@ -2,4 +2,13 @@
   plugins.lazygit = {
     enable = true;
   };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>lg";
+      action = "<CMD>LazyGit<CR>";
+      options.desc = "Open LazyGit";
+    }
+  ];
 }
